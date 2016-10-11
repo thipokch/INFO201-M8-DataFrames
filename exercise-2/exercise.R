b3f29a948887d72fd5784bd5608f80658c817eab
Thipok Ch <thipokch@D-10-18-219-84.dhcp4.washington.edu>
2016-10-11 14:36:34 -0700

    Ex2. Complete

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,22 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-
+gain <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-
+lost <- c(10, 3, 18, 17)
 
 # Combine your two vectors into a dataframe
-
+scores <- data.frame(gain = gain, lost = lost)
 
 # Create a new column "diff" that is the difference in points
-
+scores$diff <- abs(gain - lost)
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+scores$status <- gain > lost
 
 # Create a vector of the opponents
-
+opponents <- c('dolphins', 'rams', '49ers', 'jets')
 
 # Assign your dataframe rownames of their opponents
-
+rownames(scores) <-  opponents
